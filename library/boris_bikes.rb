b2c4ef573cb96917b710de759a0a5ebd22f923f1
Taj <tajbi21@gmail.com>
2019-05-15 14:27:06 +0100

    boris_bike_tuesday

--- library/boris_bikes.rb
+++ library/boris_bikes.rb
@@ -1,14 +1,24 @@
 class DockingStation
   attr_reader :bike_released
   attr_accessor :bike1
+  attr_accessor :storage
 
   def initialize
+    @storage = []
     @bike_released = false
+  end
+
+  def dock_bike(bike)
+  @storage.push(bike)
   end
 
   def release_bike
     @bike_released  = true
     @bike1 = Bike.new
+  end
+
+  def see_storage
+    @storage
   end
 end
 
@@ -16,7 +26,8 @@ class Bike
   def working?
   end
 end
+
 docking_station = DockingStation.new
 docking_station.release_bike
-newbike = Bike.new
-newbike.working?
+docking_station.dock_bike(docking_station.bike1)
+docking_station.see_storage
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,4 +9,23 @@ describe DockingStation do
    #assertion
    expect(docking_station.bike_released).to be true
  end
+
+ it 'can dock bike at the docking station' do
+   #arrange
+   docking_station = DockingStation.new
+   docking_station.release_bike
+   #Act
+   docking_station.dock_bike(docking_station.bike1)
+   #assertion
+   expect(docking_station.storage).to include(docking_station.bike1)
+ end
+
+ it 'can check if the bike has been docked' do
+   #Arrange
+   docking_station = DockingStation.new
+   docking_station.release_bike
+   #Act
+   docking_station.dock_bike(docking_station.bike1)
+   expect(docking_station.see_storage).to include(docking_station.bike1)
+ end
 end
